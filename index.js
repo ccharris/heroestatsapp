853c1caa59b5caa1608e1c5d1344fd8b97e8d941
Carolyn Harris <charris@scx-mbp-81.local>
2017-09-04 17:17:23 -0400

    add in back correct port

--- index.js
+++ index.js
@@ -37,4 +37,4 @@ app.post('/heroDailyStat', (req, res) => {
     })
 })
 
-app.listen(3001, () => console.log('Server running on port 3000'))
+app.listen(3000, () => console.log('Server running on port 3000'))
