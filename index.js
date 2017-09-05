60946837f2992ee893a95c161491705a2e41ab62
Carolyn Harris <charris@scx-mbp-81.local>
2017-09-05 14:25:07 -0400

    better rewrite soon

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const gameModes = ['QuickMatch', 'TeamLeague', 'HeroLeague'];
 
 
 app.use(cors())
+app.use(bodyParser())
 app.get('/heroDailyStat', (req, res) => {
   store
     .getHeroDailyStat({
@@ -32,21 +33,22 @@ app.get('/getAllHeroesStats', (req, res) => {
 app.post('/heroDailyStat', (req, res) => {
         let heroesStats = [];
         let gameStats = [];
-        let day = req.day
+        let day = req.body.params.day
+        let endDay = moment(day).add(1, 'days').format('YYYY-MM-DD hh:mm:ss')
         let counter = 0;
         let more = false;
         axios.get('http://hotsapi.net/api/v1/replays', {
             params: {   
                 start_date: day,
-                end_date: moment(day).add(1, 'days')
+                end_date: endDay
             }
         })
         .then(function (response) {
-            if (response.length >= 1) {
-                heroesStats.push(response);
-                if (response.length = 100) {
-                  counter = response[response.length-1].id;
-                  more = true;  
+            if (response.data.length >= 1) {
+                console.log(response.data.length);
+                heroesStats.push(response.data);
+                if (response.data.length = 100) {
+                  counter = response.data[response.data.length-1].id;
                 } else {
                     counter = 0;
                 }
@@ -55,71 +57,70 @@ app.post('/heroDailyStat', (req, res) => {
         .catch(function (error) {
             console.log(error);
         });
-        while(more){
-            if(counter > 0){
-                axios.get('http://hotsapi.net/api/v1/replays', {
-                    params: {
-                        start_date: day,
-                        end_date: moment(new Date()),
-                        min_id: counter
-                    }
-                })
-                .then(function (response) {
-                    if (response.length >= 1) {
-                        heroesStats.push(response);
-                        if (response.length = 100) {
-                        counter = response[response.length-1].id;  
-                        } else {
-                            counter = 0;
-                            more = false;
-                        }
-                    }
-                })
-                .catch(function (error) {
-                    console.log(error);
-                });
-            }
-        }
-
-        for(game in heroesStats) {
-           axios.get(`http://hotsapi.net/api/v1/replays/${game.id}`)
+        if(counter > 0){
+            axios.get('http://hotsapi.net/api/v1/replays', {
+                params: {
+                    start_date: day,
+                    end_date: endDay,
+                    min_id: counter
+                }
+            })
             .then(function (response) {
-                gameStats.push(response);
+                if (response.data.length >= 1) {
+                    heroesStats.push(response.data);
+                    if (response.data.length = 100) {
+                    counter = response.data[response.data.length-1].id;  
+                    } else {
+                        counter = 0;
+                    }
+                }
             })
             .catch(function (error) {
                 console.log(error);
             });
         }
-
-        for (hero in heroes){
-            let current = gameStats.filter((n) => n.players.hero == hero);
-            for (gameMode in gameModes){
-                let modeGames = current.filter((o) => o.game_type == gameMode);
-                let gameCounter = 0;
-                let winCounter = 0;
-                for (game in modeGames) {
-                    gameCounter++;
-                    let onlyHero = game.players.filter((p) => p.hero == hero);
-                    if (onlyHero.winner == true) {
-                        winCounter++;
-                    }
-                }
-                store
-                .createHeroDailyStat({
-                    hero: hero,
-                    date: day,
-                    wins: winCounter,
-                    games: gameCounter,
-                    gameType: gameMode,
-                    winRate: ((winCounter)/(gameCounter))
-                })
-                .then((heroesStats) => {
-                res.status(200).json(heroesStats);
-                })
-            }
-            
-        }
+        // console.log(heroesStats);
+        
         
 })
+//rewrite functionally
+// putinDb = () => {
+//     for(game in heroesStats) {
+//            axios.get(`http://hotsapi.net/api/v1/replays/${game.id}`)
+//             .then(function (response) {
+//                 gameStats.push(response.data);
+//             })
+//             .catch(function (error) {
+//                 console.log(error);
+//             });
+//         }
 
-app.listen(3000, () => console.log('Server running on port 3000'))
+//         for (hero in heroes){
+//             let current = gameStats.filter((n) => n.players.hero == hero);
+//             for (gameMode in gameModes){
+//                 let modeGames = current.filter((o) => o.game_type == gameMode);
+//                 let gameCounter = 0;
+//                 let winCounter = 0;
+//                 for (game in modeGames) {
+//                     gameCounter++;
+//                     let onlyHero = game.players.filter((p) => p.hero == hero);
+//                     if (onlyHero.winner == true) {
+//                         winCounter++;
+//                     }
+//                 }
+//                 store
+//                 .createHeroDailyStat({
+//                     hero: hero,
+//                     date: day,
+//                     wins: winCounter,
+//                     games: gameCounter,
+//                     gameType: gameMode,
+//                     winRate: ((winCounter)/(gameCounter))
+//                 })
+//             }
+            
+//         }
+
+// }
+
+app.listen(3002, () => console.log('Server running on port 3000'))
